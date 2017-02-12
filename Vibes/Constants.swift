d0dac7544472f9005f6a0678c34b46355052dd5e
Benjamin Katz <benjaminkatz@Benjamins-MBP.fios-router.home>
2017-02-12 14:21:04 -0500

    Git init commit

---
+++ Vibes/Constants.swift
@@ -0,0 +1,26 @@
+//
+//  Constants.swift
+//  Vibes
+//
+//  Created by Benjamin Katz on 2/7/17.
+//  Copyright © 2017 Benjamin Katz. All rights reserved.
+//
+
+import Foundation
+
+// Question Constants
+let question1 = "What type of Vibe are you looking for?"
+let question2 = "What type of venue would you prefer?"
+let question3 = "Do you want food?"
+let question4 = "Select your price range"
+let question5 = "How far are you willing to travel from your current location?"
+
+let questionArray = [question1, question2, question3, question4, question5]
+
+// Answer Constants
+let q1Answers = ["Rage", "Chill", "Romantic", "Low Key", "College", "Mystery", "Adventure", "Pregame","Older", "Younger"]
+let q2Answers = ["Bar", "Club", "Music Venue", "Performance", "Brewery", "Sports Bar", "Strip Club"]
+let q3Answers = ["Yes", "No"]
+let q4Answers = ["Doesn't matter", "Dirt cheap", "Cheap", "Average (for the area)", "Fancy", "Wear a fucking suit", "Had to sacrifice a lamb for this drink"]
+let q5Answers = ["Fuck you, bring the drink to me (0-1 miles)", "As far as I can roll out of bed (1-3 miles)", "Fine, I'll walk a bit (3-10)", "Hope this Uber driver knows where he's going (10-20) miles", "I live in bumfuck nowhere save me please (20-50 miles)", "I have a problem, and drinking is the solution (50+ miles)"]
+
---
+++ Vibes/QuestionVC.swift
@@ -0,0 +1,34 @@
+//
+//  QuestionVC.swift
+//  Vibes
+//
+//  Created by Benjamin Katz on 2/7/17.
+//  Copyright © 2017 Benjamin Katz. All rights reserved.
+//
+
+import UIKit
+
+class QuestionVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        <#code#>
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        <#code#>
+    }
+    
+
+
+}
+
